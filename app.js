558ae79e14cb246b3bc1deaea1d6accf948c8f91
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-08-09 00:56:05 +0500

    task complete

--- app.js
+++ app.js
@@ -2,3 +2,12 @@
 
 // var dateNow = new Date();
 // document.getElementById("date").innerHTML = dateNow;
+
+
+//************ Question 2 ************// 
+
+// var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug","Sep","Oct","Nov","Dec"];
+// var dateNow = new Date();
+// var month = dateNow.getMonth();
+// var monthes = monthNames[month];
+// alert("Current Month : " + monthes);
